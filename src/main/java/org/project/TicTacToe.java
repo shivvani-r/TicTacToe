ac0bae8491ccffde086bff9fb35a572741bf6cb6
shivvani <shivvani@Carrots-Mac.local>
2025-11-10 23:27:32 +0530

    Update file path

--- src/main/java/org/project/TicTacToe.java
+++ src/main/java/org/project/TicTacToe.java
@@ -1,15 +1,15 @@
-package ticTacToe;
+package org.project;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 public class TicTacToe extends Application {
 
